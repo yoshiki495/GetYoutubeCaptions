06cfcf8b9cc8dd1238b819f0831df20cb65716bf
yoshiki <tanayoshi78907890@icloud.com>
2022-06-30 23:40:24 +0900

    add get-youtube function

---
+++ src/main.py
@@ -0,0 +1,10 @@
+
+from youtube_transcript_api import YouTubeTranscriptApi
+  
+# assigning srt variable with the list
+# of dictonaries obtained by the get_transcript() function
+srt = YouTubeTranscriptApi.get_transcript("3inFWGBsrKc&t=193s", languages=['en'])
+  
+# prints the result
+for i in srt:
+    print(i['text'])
