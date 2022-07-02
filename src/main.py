bf6d71e85fa095b86cb85568a5dadfaf29588ac9
yoshiki <tanayoshi78907890@icloud.com>
2022-07-02 21:11:41 +0900

    make it better

--- src/main.py
+++ src/main.py
@@ -1,10 +1,7 @@
-
 from youtube_transcript_api import YouTubeTranscriptApi
   
 # assigning srt variable with the list
 # of dictonaries obtained by the get_transcript() function
 srt = YouTubeTranscriptApi.get_transcript("3inFWGBsrKc&t=193s", languages=['en'])
-  
-# prints the result
-for i in srt:
-    print(i['text'])
+
+print(srt)
