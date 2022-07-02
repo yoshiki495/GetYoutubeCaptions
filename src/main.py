75a66af691999d984636fe0b13471ab0110429ab
yoshiki <tanayoshi78907890@icloud.com>
2022-07-02 22:52:25 +0900

    fix

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
+from flask import Flask, render_template, url_for, request, redirect
 from youtube_transcript_api import YouTubeTranscriptApi
   
-# assigning srt variable with the list
-# of dictonaries obtained by the get_transcript() function
-srt = YouTubeTranscriptApi.get_transcript("3inFWGBsrKc&t=193s", languages=['en'])
+app = Flask(__name__)
 
-print(srt)
+@app.route('/get', methods=['GET'])
+def get():
+    # assigning srt variable with the list
+    # of dictonaries obtained by the get_transcript() function
+    youtube_id = request.args.get('id')
+    srt = YouTubeTranscriptApi.get_transcript("3inFWGBsrKc&t=193s", languages=['en'])
+    return srt
