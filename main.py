9d0a7d167667f8149f474418aaf5045aaa54083c
yoshiki <tanayoshi78907890@icloud.com>
2022-07-04 15:57:25 +0900

    fix

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, url_for, request, redirect
 from youtube_transcript_api import YouTubeTranscriptApi
+from flask_cors import CORS
   
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/get', methods=['GET'])
 def get():
