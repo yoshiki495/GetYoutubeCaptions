8ef72c896d4faf6e994dbe9e84b3f3566281cd41
yoshiki <tanayoshi78907890@icloud.com>
2022-07-07 00:41:17 +0900

    fix

--- lambda_function.py
+++ lambda_function.py
@@ -2,8 +2,16 @@ import json
 from youtube_transcript_api import YouTubeTranscriptApi
 
 def lambda_handler(event, context):
-    # TODO implement
-    res = YouTubeTranscriptApi.get_transcripts(['WK4SwQaPUXM'])
+    list = []
+    youtube_id = event['queryStringParameters']['param']
+    get_data = YouTubeTranscriptApi.get_transcripts([youtube_id])
+    get_list = []
+    for i in get_data[0][youtube_id]:
+        get_list.append(i['text'])
+    str_data = ' '.join(get_list)
+    dict = {'text': str_data }
+    list.append(dict)
+    res_data = get_data[0][youtube_id]
     return {
         'statusCode': 200,
         'headers': {
@@ -11,5 +19,5 @@ def lambda_handler(event, context):
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'GET'
         },
-        'body': json.dumps(res)
+        'body': json.dumps(list)
     }
